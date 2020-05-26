34d908fdee1e453ef23b982264e470707c940a19
Itamichan <cristinagarbuz@gmail.com>
2020-05-26 17:20:07 +0200

    added Router in the Layout comp

--- front_end/src/Components/Layout/Layout.js
+++ front_end/src/Components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./Layout.scss";
 import Navigation from "../Navigation/Navigation";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 const Layout = (props) => {
     return (
         <div>
-           <Navigation/>
+            <Router>
+                <Navigation/>
+            </Router>
         </div>
     )
 };
