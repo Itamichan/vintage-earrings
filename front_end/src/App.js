50768966ed2718b5b4b3aceb4a7759b9a126d04c
Itamichan <cristinagarbuz@gmail.com>
2020-05-26 16:39:45 +0200

    created Layout comp

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.scss';
+import Layout from "./Components/Layout/Layout";
 
 function App() {
     return (
         <div className="app, text-header-important">
-           hello, I am a vintage earrings shop!
-          <div className={"text-default"}>
-            I am simple text. i need more text to have a better understanding of style
-          </div>
-          <button className={"action-button"}>text</button>
+            <Layout/>
+            hello, I am a vintage earrings shop!
         </div>
     );
 }
---
+++ front_end/src/Components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Layout.scss";
+
+const Layout = (props) => {
+    return (
+        <div>
+            I am the layout
+        </div>
+    )
+};
+
+export default Layout;
