98ad1500adfde690cfab875cc936e048a5b85acb
Itamichan <cristinagarbuz@gmail.com>
2020-05-27 10:12:44 +0200

    style the navigation comp

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -13,7 +13,6 @@ function App() {
             <Provider store={store}>
                 <Layout/>
             </Provider>
-            hello, I am a vintage earrings shop!
         </div>
     );
 }
