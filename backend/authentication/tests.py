6478a91a79bcb24d5f7dbe5c133039ef92b361c3
Itamichan <cristinagarbuz@gmail.com>
2020-06-02 11:57:47 +0200

    created tests for the LoginView

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -1,12 +1,14 @@
 import json
 
+import jwt
+from django.conf import settings
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.hashers import make_password
 from user.models import User
 
 
-class Registration(TestCase):
+class RegistrationTest(TestCase):
     def test_valid_registration_data(self):
         """
         tests that on the correct provided data a 200 response is returned.
@@ -20,15 +22,7 @@ class Registration(TestCase):
             content_type="application/json")
 
         self.assertEqual(response.status_code, 200)
-
-    def test_user_creation(self):
-        """
-        tests that a user is added to the database.
-        """
-        user = User.objects.create_user('cristinagarbuz@gmail.com', 'cristinagarbuz@gmail.com', 'private2487')
-
-        self.assertEqual(user.username, 'cristinagarbuz@gmail.com')
-        self.assertEqual(user.email, 'cristinagarbuz@gmail.com')
+        self.assertTrue(User.objects.filter(email="cristinagarbuz@gmail.com").exists())
 
     def test_user_creation_existing_username(self):
         """
@@ -129,3 +123,58 @@ class Registration(TestCase):
             content_type="application/json")
 
         self.assertEqual(response.status_code, 400)
+
+
+class LoginTest(TestCase):
+    def test_token_creation(self):
+        """
+        tests that on user login a 200 status code is returned. As well checks that the response contains a token
+        and that the user id from the token corresponds with the existing user id in the database.
+        """
+        user = User.objects.create_user(username='cristinagarbuz@gmail.com', password="private2487")
+
+        response = self.client.post(
+            path='/api/v1/login',
+            data=json.dumps({
+                "password": "private2487",
+                "email": "cristinagarbuz@gmail.com",
+            }),
+            content_type="application/json")
+
+        self.assertEqual(response.status_code, 200)
+
+        # we use .json() to get the json payload from the response
+        self.assertIn('token', response.json())
+
+        token = response.json()['token']
+
+        jwt_payload = jwt.decode(token, settings.SECRET_KEY, verify=True)
+        user_id_from_token = jwt_payload["id"]
+
+        self.assertEqual(user_id_from_token, user.id)
+
+    def test_invalid_credentials_on_login(self):
+        """
+        tests that an error is raised if the user provided incorrect password or email.
+        """
+        user = User.objects.create_user(username='cristinagarbuz@gmail.com', password="private2487")
+
+        response = self.client.post(
+            path='/api/v1/login',
+            data=json.dumps({
+                "password": "privat2487",
+                "email": "cristinagarbuz@gmail.com",
+            }),
+            content_type="application/json")
+
+        self.assertEqual(response.status_code, 401)
+
+        response = self.client.post(
+            path='/api/v1/login',
+            data=json.dumps({
+                "password": "private2487",
+                "email": "cristinagarb@gmail.com",
+            }),
+            content_type="application/json")
+
+        self.assertEqual(response.status_code, 401)
