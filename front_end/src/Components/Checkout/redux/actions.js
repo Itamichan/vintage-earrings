b8151a001c16b39f30a407277d3df366f5bb0603
Itamichan <cristinagarbuz@gmail.com>
2020-06-06 21:01:57 +0200

    store basket_id in local storage via BasketReducer

--- front_end/src/Components/Checkout/redux/actions.js
+++ front_end/src/Components/Checkout/redux/actions.js
@@ -1,3 +1,9 @@
+export const Basket = (basket_id) => {
+    return {
+        type: "BASKET",
+        basket_id: basket_id,
+    }
+};
 
 export const AddToBasket = (product_id) => {
     return {
--- front_end/src/Components/Checkout/redux/reducer.js
+++ front_end/src/Components/Checkout/redux/reducer.js
@@ -1,6 +1,5 @@
 const initialState = {
-    basketItems: [],
-
+    basketItems: []
 };
 
 const BasketReducer = (state, action) => {
@@ -8,15 +7,22 @@ const BasketReducer = (state, action) => {
     if (typeof state === "undefined") {
         return initialState
     }
-    if (action.type === "ADD_TO_BASKET") {
-        let basketItemsList = state.basketItems.push(action.product_id);
-        return {
-            //unwraps the state dict
-            ...state,
-            basketItems: basketItemsList
-        };
-    } else {
-        return state
+    switch (action.type) {
+        case  "BASKET":
+            //store in our local storage the id of created basket
+            localStorage.setItem("basket_id", action.basket_id);
+            return {
+                //unwraps the state dict
+                ...state
+            };
+        case  "ADD_TO_BASKET":
+            let basketItemsList = state.basketItems.push(action.product_id);
+            return {
+                ...state,
+                basketItems: basketItemsList
+            };
+        default:
+            return state
     }
 };
 
