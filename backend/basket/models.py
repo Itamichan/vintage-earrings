e9d69e89b86943423d5d59667aac8b01874ad359
Itamichan <cristinagarbuz@gmail.com>
2020-06-08 14:05:10 +0200

    created Basket model with uuid pk for security

--- backend/basket/models.py
+++ backend/basket/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Basket(models.Model):
+    # Models the basket.
+
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True,  editable=False)
+
+    class Meta:
+        db_table = 'baskets'
--- backend/basket/tests.py
+++ backend/basket/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+from basket.models import Basket
+
+
+class BasketCreationTest(TestCase):
+    def test_basket_exists_in_the_database(self):
+        response = self.client.get(path='/api/v1/baskets/')
+
+        basket_id = Basket.objects.first().id
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['basket_id'], str(basket_id))
