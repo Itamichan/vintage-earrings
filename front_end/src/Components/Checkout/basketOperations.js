91090fd3e8911a816e8c37b35703bcf01da0805e
Itamichan <cristinagarbuz@gmail.com>
2020-06-09 16:11:14 +0200

    created a function which updates the nr of item in the backend and frontend

--- front_end/src/Components/Checkout/basketOperations.js
+++ front_end/src/Components/Checkout/basketOperations.js
@@ -1,9 +1,9 @@
 import {store} from "../../store";
 import axios from "axios";
-import {addToBasket} from "./redux/actions";
+import {addToBasket, updateBasket} from "./redux/actions";
 
 
-export const addProduct = async (productId) => {
+export const addItem = async (productId) => {
 
     let basketId = localStorage.getItem("basket_id");
 
@@ -11,11 +11,23 @@ export const addProduct = async (productId) => {
     if (!basketId) {
         let {data} = await axios.post(`api/v1/baskets/`);
         localStorage.setItem("basket_id", data.basket_id);
+        //updating the value of the baskedId
         basketId = data.basket_id
     }
 
     let {data} = await axios.post(`api/v1/baskets/${basketId}/items/`, {
         'product_id': productId
     });
+    //adds item to the basketItems in the BasketReducer.
     store.dispatch(addToBasket(data))
+};
+
+export const increaseItem = async (productId) => {
+    let basketId = localStorage.getItem("basket_id");
+
+    await axios.patch(`api/v1/baskets/${basketId}/items/`, {
+        'product_id': productId
+    });
+
+    store.dispatch(updateBasket(productId))
 };
