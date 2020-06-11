2a601d7adc195cb6497a93470c7174045ea6fe95
Itamichan <cristinagarbuz@gmail.com>
2020-06-11 10:52:28 +0200

    toggle the view of Basket items based on presence or absence of items

--- front_end/src/Components/Basket/Basket.js
+++ front_end/src/Components/Basket/Basket.js
@@ -1,41 +1,60 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Col, Container, Row} from 'reactstrap';
 import {connect} from "react-redux";
 import {withRouter} from "react-router";
 import BasketItem from "./BasketItem";
+import {removeItem} from "./basketOperations";
 
 const Basket = ({showItemsCount, showItemsTotal, basketItems}) => {
+
 
     let itemsList = basketItems.map(item => {
         return (
             <Col xs={"12"} key={item.id}>
                 <BasketItem
                     cardTitle={item.product.name}
+                    idemId={item.id}
                     itemImgList={item.product.photos}
                     itemPrice={`${item.product.price} €`}
                     itemQuantity={item.items_quantity}
+                    onTrashClick={() => removeItem(item.id)}
                 />
             </Col>
         )
     });
 
+
     return (
         <div className={'start-point'}>
             basket
             <Container>
-                <Row>
-                    <Col>
-                        <div>
-                            Total ({showItemsCount} items):
-                        </div>
-
-                    </Col>
-                    <Col>
-                        <div>
-                            {showItemsTotal} €
-                        </div>
-                    </Col>
-                </Row>
+                {showItemsCount > 0? (
+                    <Row>
+                        <Col>
+                            <div>
+                                <span>Total ({showItemsCount} </span>
+                                {
+                                    showItemsCount > 1 ? (
+                                        <span>items):</span>
+                                    ) : (
+                                        <span>item):</span>)}
+                            </div>
+                        </Col>
+                        <Col>
+                            <div>
+                                {showItemsTotal} €
+                            </div>
+                        </Col>
+                    </Row>
+                ) : (
+                    <Row>
+                        <Col xs={12}>
+                            <div>
+                               You have no items in your basket
+                            </div>
+                        </Col>
+                    </Row>
+                )}
                 <Row>
                     {itemsList}
                 </Row>
@@ -47,9 +66,7 @@ const Basket = ({showItemsCount, showItemsTotal, basketItems}) => {
 //dispatch will move the provided action dict (result of login(token))
 // to the global state and will run the reducer with the provided action
 const mapDispatchToProps = (dispatch) => {
-    return {
-
-    }
+    return {}
 };
 
 //map the global state to properties that are passed into the comp
