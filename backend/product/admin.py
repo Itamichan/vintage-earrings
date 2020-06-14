b227562af81678e229e72976500c0f5fcb323324
Itamichan <cristinagarbuz@gmail.com>
2020-06-14 20:55:00 +0200

    show the Product and ProductPhoto in the admin view

--- backend/product/admin.py
+++ backend/product/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Product, ProductPhoto
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'quantity']
+
+
+class ProductPhotoAdmin(admin.ModelAdmin):
+    list_display = ['photo_url']
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductPhoto, ProductPhotoAdmin)
