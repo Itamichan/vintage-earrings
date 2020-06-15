37b6e871bb070e4aab16859b83e9597f73a0c8dd
Itamichan <cristinagarbuz@gmail.com>
2020-06-15 13:49:46 +0200

    cancellationPage comp renders the view in case of payment cancellation

--- front_end/src/Components/Checkout/CancellationPage.js
+++ front_end/src/Components/Checkout/CancellationPage.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {Button} from "reactstrap";
+import {withRouter} from "react-router";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
-const CancellationPage = (props) => {
+//todo prettify the page
+const CancellationPage = ({history}) => {
     return (
-        <div>
-            cancelled payment
+        <div className={'start-point'}>
+            Your checkout process is cancelled.
+            <div>
+                Continue shopping:
+                <Button onClick={() => history.push('/products')}>Shop Now!</Button>
+            </div>
+            <div>
+                Go back to your basket:
+                <div onClick={() => history.push("/basket")}>
+                    <FontAwesomeIcon icon="shopping-cart"/>
+                </div>
+            </div>
         </div>
     )
 };
 
-export default CancellationPage;
+export default withRouter(CancellationPage);
