cb9935bead3c2238905ca988aa6665272590283d
Itamichan <cristinagarbuz@gmail.com>
2020-06-16 15:47:40 +0200

    created the order consumer which creates a Order from a Basket

---
+++ backend/order/management/commands/consume_order.py
@@ -0,0 +1,47 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand, CommandError
+
+import pika
+
+from basket.models import Basket, BasketItem
+from order.models import Order, OrderItem
+
+
+class Command(BaseCommand):
+    help = 'Creates the Order and destroys the Basket'
+
+    def handle(self, *args, **options):
+        credentials = pika.PlainCredentials(settings.MPQ_USERNAME, settings.MPQ_PASSWORD)
+
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(host='localhost', credentials=credentials))
+        channel = connection.channel()
+
+        channel.queue_declare(queue='order', durable=True)
+
+        def callback(ch, method, properties, body):
+
+            basket = Basket.objects.get(pk=body.decode('ascii'))
+
+            print(basket)
+
+            items_qs = BasketItem.objects.select_related('product').filter(basket=basket)
+
+            order = Order.objects.create(stripe_id=basket.stripe_id)
+
+            for item in items_qs:
+                OrderItem.objects.create(order=order, product=item.product, items_quantity=item.items_quantity)
+
+            # destroy the basket after the order is created
+            basket.delete()
+
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+            # This uses the basic.qos protocol method to tell RabbitMQ not to give more than one message to a worker at a time.
+            channel.basic_qos(prefetch_count=1)
+
+            channel.basic_consume(
+                queue='order', on_message_callback=callback)
+
+            print(' [*] Waiting for messages. To exit press CTRL+C')
+            channel.start_consuming()
