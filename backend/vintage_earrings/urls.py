a7cde7d8b07ca90a110e6f42da94c52372cb78c0
Itamichan <cristinagarbuz@gmail.com>
2020-06-17 16:24:27 +0200

    added paths to redirect to index page

--- backend/vintage_earrings/urls.py
+++ backend/vintage_earrings/urls.py
@@ -1,6 +1,7 @@
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+
+from frontend.views import frontend
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,4 +11,10 @@ urlpatterns = [
     path('api/v1/products/', include('product.urls')),
     path('api/v1/baskets/', include('basket.urls')),
     path('api/v1/orders/', include('order.urls')),
+]
+
+# Route to the frontend if nothing else matches
+urlpatterns += [
+    path(r'', frontend, name="frontend"),
+    re_path(r'^.*/$', frontend, name="frontend"),
 ]
