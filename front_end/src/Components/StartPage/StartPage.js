50294a23978f4696d57e6efbee861be3c6915999
Itamichan <cristinagarbuz@gmail.com>
2020-06-22 09:56:50 +0200

    added the text-header on the home-page

--- front_end/src/Components/StartPage/StartPage.js
+++ front_end/src/Components/StartPage/StartPage.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import "./StartPage.scss";
-import {Button, Label, Modal, ModalBody, ModalHeader} from "reactstrap";
+import {Button, Col, Container, Label, Modal, ModalBody, ModalHeader, Row} from "reactstrap";
 import {withRouter} from "react-router";
+import {
+    BrowserView,
+    MobileView,
+    isBrowser,
+    isMobile
+} from "react-device-detect";
 import "./StartPage.scss";
 
 const StartPage = ({history}) => {
@@ -10,9 +16,9 @@ const StartPage = ({history}) => {
             <section id={'hero-container'}>
                 <div id={'img-container'}/>
                 <div id={'img-header'}>
-                    <h1 className={'text-header'} id={'hero-header'}>
+                    <div className={'text-header'} id={'hero-header'}>
                         Find your perfect pair...
-                    </h1>
+                    </div>
                 </div>
                 <Button
                     id={'shop-button'}
@@ -22,7 +28,24 @@ const StartPage = ({history}) => {
                     Shop Now!
                 </Button>
             </section>
-
+            <section id={'logo-text-section'}>
+                <Container fluid={true} id={'logo-text-container'}>
+                    <Row>
+                        <Col>
+                            <BrowserView>
+                                <div className={'text-header-important'}>
+                                    Here we offer unique style earrings for the unique person you are!
+                                </div>
+                            </BrowserView>
+                            <MobileView>
+                                <div className={'text-header-important'}>
+                                    Unique style earrings for you!
+                                </div>
+                            </MobileView>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
         </div>
     )
 };
