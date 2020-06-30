d7647fe81f5aef784dedff07bc78b67f51459e85
Itamichan <cristinagarbuz@gmail.com>
2020-06-30 21:53:02 +0200

    removed the dead code linked to the order view

--- backend/vintage_earrings/urls.py
+++ backend/vintage_earrings/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('api/v1/user/', include('user.urls')),
     path('api/v1/products/', include('product.urls')),
     path('api/v1/baskets/', include('basket.urls')),
-    path('api/v1/orders/', include('order.urls')),
 ]
 
 # Route to the frontend if nothing else matches
