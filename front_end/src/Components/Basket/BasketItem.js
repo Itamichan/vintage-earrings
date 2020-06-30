159942d8d77409faf80bd040db0d2947fa02418a
Itamichan <cristinagarbuz@gmail.com>
2020-06-30 22:32:15 +0200

    ask delete confirmation on removal of the item from the basket

--- front_end/src/Components/Basket/BasketItem.js
+++ front_end/src/Components/Basket/BasketItem.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {Col, FormGroup, Input, Label, Media, Row} from "reactstrap";
+import {Button, Col, FormGroup, Input, Label, Media, Modal, ModalBody, Row} from "reactstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {updateItem} from "./basketOperations";
 import "./BasketItem.scss";
@@ -8,6 +8,7 @@ import {withRouter} from "react-router";
 const BasketItem = ({history, item, onTrashClick}) => {
 
     const [sendingRequest, setSendingRequest] = useState(false);
+    const [showRemoveItem, setShowRemoveItem] = useState(false);
 
     const updateItemQuantity = (newQuantity) => {
         try {
@@ -24,70 +25,95 @@ const BasketItem = ({history, item, onTrashClick}) => {
     };
 
     return (
-        <Media className={"media-container"}>
-            <Media
-                left
-                className={"media-img"}
-                object
-                src={item.product.photos[0].photo_url}
-                alt="product image"
-                onClick={() => history.push({
-                    pathname: `/products/product/${item.product.id}`,
-                    state: {
-                        product: item.product
-                    }
-                })}
+        <div>
+            <Media className={"media-container"}>
+                <Media
+                    left
+                    className={"media-img"}
+                    object
+                    src={item.product.photos[0].photo_url}
+                    alt="product image"
+                    onClick={() => history.push({
+                        pathname: `/products/product/${item.product.id}`,
+                        state: {
+                            product: item.product
+                        }
+                    })}
+                >
+                </Media>
+                <Media body className={"media-body"}>
+                    <Row>
+                        <Col
+                            xs={"7"}
+                            className={"attraction-info-body text-highlight"}
+                        >
+                            <Row>
+                                <Col>
+                                    <Media heading className={"text-header-standard media-heading"}>
+                                        {item.product.name}
+                                    </Media>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col xs={8} md={4}>
+                                    <FormGroup>
+                                        <Label for="itemQty" className={'text-default'}>Quantity</Label>
+                                        <Input type="select" name="itemQty" label="Quantity" id="itemQty"
+                                               value={item.items_quantity} disabled={sendingRequest}
+                                               onChange={(e) => updateItemQuantity(parseInt(e.target.value))}
+                                        >
+                                            <option>1</option>
+                                            <option>2</option>
+                                            <option>3</option>
+                                            <option>4</option>
+                                            <option>5</option>
+                                            <option>6</option>
+                                            <option>7</option>
+                                            <option>8</option>
+                                            <option>9</option>
+                                            <option>10</option>
+                                        </Input>
+                                    </FormGroup>
+                                </Col>
+                                <Col xs={4} md={12} className={'trash-icon-wrapper'}>
+                                    <div onClick={() => setShowRemoveItem(true)} className={"text-default icon-delete"}>
+                                        <FontAwesomeIcon size={"2x"} icon={['far', 'trash-alt']}/>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Col>
+                        <Col xs={"5"}
+                             className={'item-price text-header-standard'}
+                        >
+                            {`${item.product.price} €`}
+                        </Col>
+                    </Row>
+                </Media>
+            </Media>
+            {showRemoveItem &&
+            <Modal isOpen={showRemoveItem}
+                   centered={true}
+                   id={'addresses-modal'}
             >
-            </Media>
-            <Media body className={"media-body"}>
-                <Row>
-                    <Col
-                        xs={"7"}
-                        className={"attraction-info-body text-highlight"}
-                    >
-                        <Row>
-                            <Col>
-                                <Media heading className={"text-header-standard media-heading"}>
-                                    {item.product.name}
-                                </Media>
-                            </Col>
-                        </Row>
-                        <Row>
-                            <Col xs={8} md={4}>
-                                <FormGroup>
-                                    <Label for="itemQty" className={'text-default'}>Quantity</Label>
-                                    <Input type="select" name="itemQty" label="Quantity" id="itemQty"
-                                           value={item.items_quantity} disabled={sendingRequest}
-                                           onChange={(e) => updateItemQuantity(parseInt(e.target.value))}
-                                    >
-                                        <option>1</option>
-                                        <option>2</option>
-                                        <option>3</option>
-                                        <option>4</option>
-                                        <option>5</option>
-                                        <option>6</option>
-                                        <option>7</option>
-                                        <option>8</option>
-                                        <option>9</option>
-                                        <option>10</option>
-                                    </Input>
-                                </FormGroup>
-                            </Col>
-                            <Col xs={4} md={12} className={'trash-icon-wrapper'}>
-                                <div onClick={() => onTrashClick()} className={"text-default icon-delete"}>
-                                    <FontAwesomeIcon size={"2x"} icon={['far', 'trash-alt']}/>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col xs={"5"}
-                         className={'item-price text-header-standard'}
-                    >
-                        {`${item.product.price} €`}
-                    </Col>
-                </Row>
-            </Media>
-        </Media>
+                <ModalBody className={'delete-confirmation-container'}>
+                    <div className={'modal-question text-header-standard'}>Are you sure?</div>
+                    <div className={'buttons-container'}>
+                        <Button
+                            className={'action-button'}
+                            onClick={() => onTrashClick()}
+                        >
+                            Yes
+                        </Button>
+                        <Button
+                            className={'action-button'}
+                            onClick={() => setShowRemoveItem(false)}
+                        >
+                            No
+                        </Button>
+                    </div>
+                </ModalBody>
+            </Modal>}
+        </div>
     )
 };
 
